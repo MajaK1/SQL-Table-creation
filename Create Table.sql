64b0b488ba3de68db79456ced351df8721c2f8e4
Maja Krzemińska <96236718+MajaK1@users.noreply.github.com>
2021-12-16 21:13:02 +0100

    Add files via upload

---
+++ Create Table.sql
@@ -0,0 +1,9 @@
+create table employees(
+						id number,
+						name varchar2(30),
+						surname varchar2(60)
+						PESEL number,
+						gender varchar2(1),
+						birth_date date,
+						slary NUMBER
+						                      );
