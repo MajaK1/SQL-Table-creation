ebf8098cfb1119ef26eea3ccc8cecd2e4c8496e0
Maja Krzemińska <96236718+MajaK1@users.noreply.github.com>
2021-12-16 21:17:53 +0100

    Delete Create Table.sql

--- Create Table.sql
+++
@@ -1,9 +0,0 @@
-create table employees(
-						id number,
-						name varchar2(30),
-						surname varchar2(60)
-						PESEL number,
-						gender varchar2(1),
-						birth_date date,
-						slary NUMBER
-						                      );
